f22a051864efac1006297e6bfaf395710d258fb2
Halicade <qstr23@hotmail.com>
2026-02-14 08:44:07 -0500

    Proper null checking?

--- Source/NPS/HarmonyPatches/JobGiver_SeekSafeTemperature_TryGiveJob.cs
+++ Source/NPS/HarmonyPatches/JobGiver_SeekSafeTemperature_TryGiveJob.cs
@@ -10,7 +10,6 @@ namespace TKKN_NPS;
 //[HarmonyPatch(typeof(JobGiver_SeekSafeTemperature), "TryGiveJob")]
 internal class JobGiver_SeekSafeTemperature_TryGiveJob
 {
-
     public static void Postfix(ref Job __result, Pawn pawn) {
         if (__result != null || pawn?.RaceProps?.CanPassFences == false) {
             return;
@@ -22,37 +21,28 @@ internal class JobGiver_SeekSafeTemperature_TryGiveJob
 
         Map map = pawn.Map;
 
-        if (map.Biome != BiomeDefOf.TKKN_Oasis &&
-            !map.TileInfo.mutatorsNullable.Contains(TileMutatorDefOf.NPS_ColdSpringMutator)) {
+        if (map.Biome == BiomeDefOf.TKKN_Oasis ||
+            map.TileInfo.mutatorsNullable?.Contains(TileMutatorDefOf.NPS_ColdSpringMutator) == true) {
+            if (pawn.health.hediffSet.GetFirstHediffOfDef(RimWorld.HediffDefOf.Heatstroke)?.CurStageIndex !=
+                (int)TemperatureInjuryStage.Serious) {
+                return;
+            }
+
+            if (pawn.Position.GetTerrain(Find.CurrentMap) == TerrainDefOf.TKKN_ColdSpringsWater) {
+                __result = new Job(RimWorld.JobDefOf.Wait_SafeTemperature, 500, true);
+                return;
+            }
+
+            if (CellFinder.TryRandomClosewalkCellNear(
+                    root: pawn.Position,
+                    map: map,
+                    radius: 80,
+                    result: out var result,
+                    extraValidator: vec3 => vec3.GetTerrain(map) == TerrainDefOf.TKKN_ColdSpringsWater)) {
+                __result = new Job(RimWorld.JobDefOf.GotoSafeTemperature, result);
+            }
+
             return;
         }
-
-        var heatstroke = pawn.health.hediffSet.GetFirstHediffOfDef(RimWorld.HediffDefOf.Heatstroke);
-
-        if (heatstroke == null) {
-            return;
-        }
-
-        if (heatstroke.CurStageIndex != (int)TemperatureInjuryStage.Serious) {
-            return;
-        }
-
-        var terrain = pawn.Position.GetTerrain(Find.CurrentMap);
-        if (terrain == TerrainDefOf.TKKN_ColdSpringsWater) {
-            __result = new Job(RimWorld.JobDefOf.Wait_SafeTemperature, 500, true);
-            return;
-        }
-        
-        if (CellFinder.TryRandomClosewalkCellNear(
-                root: pawn.Position,
-                map: map,
-                radius: 80,
-                result: out var result,
-                extraValidator: vec3 => vec3.GetTerrain(map) == TerrainDefOf.TKKN_ColdSpringsWater )) {
-            __result = new Job(RimWorld.JobDefOf.GotoSafeTemperature, result);
-        }
-
-        return;
     }
-
 }
